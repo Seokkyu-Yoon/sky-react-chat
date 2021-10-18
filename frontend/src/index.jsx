6b4b5e1affb7de9cdbb0e90718a79e95bff7e592
윤석규 <yskyu93@modutech.co.kr>
2021-10-18 17:32:04 +0900

    refactor(App): change to class component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,12 +4,12 @@ import './css/index.css';
 import App from './view/App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+const element = (
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+  </React.StrictMode>
+)
+ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- frontend/src/view/App.jsx
+++ frontend/src/view/App.jsx
@@ -2,29 +2,19 @@ import React from 'react';
 import logo from '../img/logo.svg';
 import '../css/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/view/App.jsx</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
