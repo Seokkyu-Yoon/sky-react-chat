50db15c7e7fa73c65704b0a66918ea2a22e91e9b
윤석규 <yskyu93@modutech.co.kr>
2021-10-18 17:33:00 +0900

    style(index.jsx): add semicolon

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -8,7 +8,7 @@ const element = (
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
 ReactDOM.render(element, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
